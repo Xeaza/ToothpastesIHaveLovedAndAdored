87ee6a3e3da82aacd265574441c3837cf4e2c3d9
xeaza <taylorwrightsanson@gmail.com>
2014-10-16 11:33:59 -0500

    Added url connection to grab some json data

---
+++ ToothpastesIHaveLovedAndAdored/ToothpastesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToothpastesTableViewController.h
+//  ToothpastesIHaveLovedAndAdored
+//
+//  Created by Taylor Wright-Sanson on 10/16/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToothpastesTableViewController : UITableViewController
+
+@end
